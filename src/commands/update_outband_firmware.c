90c94a8db3bbbd07c433c8f82dd2e8001e6734d3
Qianbiao.NG <iampurse@vip.qq.com>
2019-05-28 22:25:03 +0800

    feat: update outband firmware

--- src/commands/update_outband_firmware.c
+++ src/commands/update_outband_firmware.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <libgen.h>
 #include "cJSON_Utils.h"
@@ -26,11 +27,11 @@ typedef struct _UpdateFirmwareOption
     char *imageURI;
     char *activateMode;
     char *firmwareType;
-    int ret;
+    int retryable;
     UtoolCommandOptionFlag flag;  /** whether the command should be executed, default yes(0) otherwise no */
 } UtoolUpdateFirmwareOption;
 
-static const char *MODE_CHOICES[] = {"Auto", "Manual", NULL};
+static const char *MODE_CHOICES[] = {UPGRADE_ACTIVATE_MODE_AUTO, UPGRADE_ACTIVATE_MODE_MANUAL, NULL};
 static const char *TYPE_CHOICES[] = {"BMC", "BIOS", "CPLD", "PSUFW", NULL};
 static const char *IMAGE_PROTOCOL_CHOICES[] = {"HTTPS", "SCP", "SFTP", "CIFS", "TFTP", "NFS", NULL};
 
@@ -51,7 +52,13 @@ static int ValidateUpdateFirmwareOptions(UtoolUpdateFirmwareOption *updateFirmwa
 static int BuildPayload(UtoolRedfishServer *server, cJSON *payload, UtoolUpdateFirmwareOption *updateFirmwareOption,
                         char **result);
 
+static int ResetBMCAndWaitingAlive(UtoolRedfishServer *server);
+
 /**
+ * 要求：升级失败要求能重试，最多3次。
+ *
+ * 如果升级BMC带了-e Auto参数，且出现BMC的HTTP不通或空间不足等情况时要求utool能直接先将BMC重启后再试。
+ * 升级过程详细记录到升级日志文件，内容及格式详见sheet "升级日志文件"。
  *
  * @param self
  * @param option
@@ -111,33 +118,57 @@ int UtoolCmdUpdateOutbandFirmware(UtoolCommandOption *commandOption, char **resu
         goto failure;
     }
 
-    // build payload
-    payload = cJSON_CreateObject();
-    ret = UtoolAssetCreatedJsonNotNull(payload);
-    if (ret != UTOOLE_OK) {
-        goto failure;
+    int retryTimes = 0;
+    while (retryTimes++ < UPGRADE_FIRMWARE_RETRY_TIMES) {
+        ZF_LOGI("Start to upgrade outband firmware now, retry round: %d", retryTimes);
+
+        if (*result != NULL) {
+            free(*result);
+        }
+
+        // build payload
+        payload = cJSON_CreateObject();
+        ret = UtoolAssetCreatedJsonNotNull(payload);
+        if (ret != UTOOLE_OK) {
+            continue;
+        }
+        ret = BuildPayload(server, payload, updateFirmwareOption, result);
+        if (ret != UTOOLE_OK || updateFirmwareOption->flag != EXECUTABLE) {
+            // TODO should we only retry when BMC return failure?
+            if (updateFirmwareOption->retryable) {
+                if (UtoolStringEquals(UPGRADE_ACTIVATE_MODE_AUTO, updateFirmwareOption->activateMode)
+                    && retryTimes != UPGRADE_FIRMWARE_RETRY_TIMES) {
+                    ZF_LOGI("Failed to upload file to BMC and activate mode is auto, will try reset BMC now.");
+                    // we do not care about whether server is alive?
+                    ResetBMCAndWaitingAlive(server);
+                }
+            }
+            continue;
+        }
+
+        char *url = "/UpdateService/Actions/UpdateService.SimpleUpdate";
+        ret = UtoolMakeCurlRequest(server, url, HTTP_POST, payload, NULL, response);
+        if (ret != UTOOLE_OK) {
+            continue;
+        }
+
+        if (response->httpStatusCode >= 400) {
+            ret = UtoolResolveFailureResponse(response, result);
+            if (ret != UTOOLE_OK) {
+                continue;
+            }
+        }
+
+        // process response
+        updateFirmwareJson = cJSON_Parse(response->content);
+        ret = UtoolAssetParseJsonNotNull(updateFirmwareJson);
+        if (ret == UTOOLE_OK) {
+            break;
+        }
     }
 
-    ret = BuildPayload(server, payload, updateFirmwareOption, result);
+    // if it still fails when reaching the maximum retries
     if (ret != UTOOLE_OK || updateFirmwareOption->flag != EXECUTABLE) {
-        goto failure;
-    }
-
-    char *url = "/UpdateService/Actions/UpdateService.SimpleUpdate";
-    ret = UtoolMakeCurlRequest(server, url, HTTP_POST, payload, NULL, response);
-    if (ret != UTOOLE_OK) {
-        goto failure;
-    }
-
-    if (response->httpStatusCode >= 400) {
-        ret = UtoolResolveFailureResponse(response, result);
-        goto failure;
-    }
-
-    // process response
-    updateFirmwareJson = cJSON_Parse(response->content);
-    ret = UtoolAssetParseJsonNotNull(updateFirmwareJson);
-    if (ret != UTOOLE_OK) {
         goto failure;
     }
 
@@ -146,8 +177,8 @@ int UtoolCmdUpdateOutbandFirmware(UtoolCommandOption *commandOption, char **resu
     if (ret != UTOOLE_OK) {
         goto failure;
     }
-    ret = UtoolBuildOutputResult(STATE_SUCCESS, output, result);
 
+    ret = UtoolBuildOutputResult(STATE_SUCCESS, output, result);
     goto done;
 
 failure:
@@ -159,6 +190,51 @@ done:
     FREE_CJSON(updateFirmwareJson)
     UtoolFreeRedfishServer(server);
     UtoolFreeCurlResponse(response);
+    return ret;
+}
+
+static int ResetBMCAndWaitingAlive(UtoolRedfishServer *server)
+{
+    int ret;
+    cJSON *payload;
+    UtoolCurlResponse *resetManagerResp = &(UtoolCurlResponse) {0};
+
+    ZF_LOGI("Restart BMC and waiting alive");
+
+    // build payload
+    char *payloadString = "{\"ResetType\" : \"ForceRestart\" }";
+    payload = cJSON_CreateRaw(payloadString);
+    ret = UtoolAssetCreatedJsonNotNull(payload);
+    if (ret != UTOOLE_OK) {
+        goto done;
+    }
+
+    // we do not care about whether reset manager is successful
+    char *url = "/Managers/%s/Actions/Manager.Reset";
+    UtoolMakeCurlRequest(server, url, HTTP_POST, payload, NULL, resetManagerResp);
+    sleep(5);  // wait 5 seconds to let manager resetting takes effect
+
+    ZF_LOGI("Waiting BMC alive now...");
+    // waiting BMC up, total wait time is (30 + 1) * 30/2
+    int interval = 30;
+    while (interval > 0) {
+        UtoolCurlResponse *getRedfishResp = &(UtoolCurlResponse) {0};
+        ret = UtoolMakeCurlRequest(server, "/", HTTP_GET, NULL, NULL, getRedfishResp);
+        if (ret == UTOOLE_OK && getRedfishResp->httpStatusCode < 300) {
+            ZF_LOGI("BMC is alive now.");
+            UtoolFreeCurlResponse(getRedfishResp);
+            break;
+        }
+
+        ZF_LOGI("BMC is down now. Next check will be %d seconds later.", interval);
+        sleep(interval);
+        interval--;
+        UtoolFreeCurlResponse(getRedfishResp);
+    }
+
+done:
+    FREE_CJSON(payload)
+    UtoolFreeCurlResponse(resetManagerResp);
     return ret;
 }
 
@@ -208,6 +284,9 @@ done:
 static int
 BuildPayload(UtoolRedfishServer *server, cJSON *payload, UtoolUpdateFirmwareOption *updateFirmwareOption, char **result)
 {
+    // reset flag
+    updateFirmwareOption->flag = EXECUTABLE;
+
     char *imageUri = updateFirmwareOption->imageURI;
 
     // only output if not command help action is requested.
@@ -220,7 +299,7 @@ BuildPayload(UtoolRedfishServer *server, cJSON *payload, UtoolUpdateFirmwareOpti
 
     int ret;
     struct stat fileInfo;
-    UtoolParsedUrl *parsedUrl;
+    UtoolParsedUrl *parsedUrl = NULL;
     UtoolCurlResponse *response = &(UtoolCurlResponse) {0};
 
     bool isLocalFile = false;
@@ -234,6 +313,7 @@ BuildPayload(UtoolRedfishServer *server, cJSON *payload, UtoolUpdateFirmwareOpti
 
     if (isLocalFile) {  /** if file exists in local machine, try to upload it to BMC */
         ZF_LOGI("Firmware image uri `%s` is a local file.", imageUri);
+
         ret = UtoolUploadFileToBMC(server, imageUri, response);
         if (ret != UTOOLE_OK) {
             goto failure;
@@ -242,6 +322,8 @@ BuildPayload(UtoolRedfishServer *server, cJSON *payload, UtoolUpdateFirmwareOpti
         if (response->httpStatusCode >= 400) {
             ZF_LOGE("Failed to upload local file `%s` to BMC.", imageUri);
             ret = UtoolResolveFailureResponse(response, result);
+            // should we only retry when BMC return failure?
+            updateFirmwareOption->retryable = 1;
             goto failure;
         }
 
