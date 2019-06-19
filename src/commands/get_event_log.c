1f6ef83e3bef258c6b70c7c53877cf8f7c5022a5
Qianbiao.NG <iampurse@vip.qq.com>
2019-06-19 22:01:27 +0800

    Feat: add export SEl log

--- src/commands/get_event_log.c
+++ src/commands/get_event_log.c
@@ -1,6 +1,6 @@
 /*
 * Copyright © Huawei Technologies Co., Ltd. 2018-2019. All rights reserved.
-* Description: command handler of `geteventlog`
+* Description: command hander for `geteventlog`
 * Author:
 * Create: 2019-06-14
 * Notes:
@@ -8,6 +8,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <libgen.h>
 #include "cJSON_Utils.h"
 #include "commons.h"
 #include "curl/curl.h"
@@ -17,32 +21,30 @@
 #include "command-interfaces.h"
 #include "argparse.h"
 #include "redfish.h"
+#include "string_utils.h"
+#include "url_parser.h"
+
+static const char *PROTOCOL_CHOICES[] = {"HTTPS", "SCP", "SFTP", "CIFS", "NFS", NULL};
+static const char *OPT_FILE_URL_REQUIRED = "Error: opt `file-uri` is required.";
+static const char *OPT_FILE_URL_ILLEGAL = "Error: option `file-uri` is illegal.";
 
 static const char *const usage[] = {
-        "geteventlog",
+        "geteventlog -u file-uri",
         NULL,
 };
 
 
-static const UtoolOutputMapping getEventMappings[] = {
-        {.sourceXpath = "/eventid", .targetKeyValue="Id"},
-        {.sourceXpath = "/level", .targetKeyValue="Severity"},
-        {.sourceXpath = "/alerttime", .targetKeyValue="EventTimestamp"},
-        {.sourceXpath = "/subjecttype", .targetKeyValue="Entity"},
-        {.sourceXpath = "/Null", .targetKeyValue="EntitySN"},
-        {.sourceXpath = "/eventsubject", .targetKeyValue="Message"},
-        {.sourceXpath = "/NUll", .targetKeyValue="MessageId"},
-        {.sourceXpath = "/eventcode", .targetKeyValue="EventId"},
-        {.sourceXpath = "/status", .targetKeyValue="Status"},
-        NULL
-};
+typedef struct _GetEventLog
+{
+    char *exportToFileUrl;
+    char *bmcTempFileUrl;
+    int isLocalFile;
 
-static const UtoolOutputMapping getEventArrayMappings[] = {
-        {.sourceXpath = "/error/@Message.ExtendedInfo/0/Oem/Huawei/SelLogEntries", .targetKeyValue="EventLog",
-         .nestMapping=getEventMappings},
-        NULL
-};
+} UtoolGetEventLog;
 
+static cJSON *BuildPayload(UtoolGetEventLog *opt, UtoolResult *result);
+
+static void ValidateSubcommandOptions(UtoolGetEventLog *opt, UtoolResult *result);
 
 /**
  * get all event logs. command handler of `geteventlog`
@@ -53,15 +55,18 @@ static const UtoolOutputMapping getEventArrayMappings[] = {
  */
 int UtoolCmdGetEventLog(UtoolCommandOption *commandOption, char **outputStr)
 {
-    int ret;
-    cJSON *output = NULL, *getLogServicesJson = NULL, *payload = NULL;
+    cJSON *output = NULL,
+            *payload = NULL,
+            *getLogServicesJson = NULL;
 
     UtoolResult *result = &(UtoolResult) {0};
     UtoolRedfishServer *server = &(UtoolRedfishServer) {0};
+    UtoolGetEventLog *opt = &(UtoolGetEventLog) {0};
 
     struct argparse_option options[] = {
             OPT_BOOLEAN('h', "help", &(commandOption->flag), HELP_SUB_COMMAND_DESC, UtoolGetHelpOptionCallback, 0, 0),
-            OPT_END(),
+            OPT_STRING ('u', "file-uri", &(opt->exportToFileUrl), "specifies path of export csv file", NULL, 0, 0),
+            OPT_END()
     };
 
     result->code = UtoolValidateSubCommandBasicOptions(commandOption, options, usage, &(result->desc));
@@ -74,15 +79,21 @@ int UtoolCmdGetEventLog(UtoolCommandOption *commandOption, char **outputStr)
         goto DONE;
     }
 
-    result->code = UtoolGetRedfishServer(commandOption, server, &(result->desc));
-    if (result->code != UTOOLE_OK || server->systemId == NULL) {
+    ValidateSubcommandOptions(opt, result);
+    if (result->interrupt) {
         goto DONE;
     }
 
-    output = cJSON_CreateObject();
-    ret = UtoolAssetCreatedJsonNotNull(output);
-    if (ret != UTOOLE_OK) {
+    // build payload
+    payload = BuildPayload(opt, result);
+    if (result->interrupt) {
         goto FAILURE;
+    }
+
+    // get redfish system id
+    result->code = UtoolGetRedfishServer(commandOption, server, &(result->desc));
+    if (result->code != UTOOLE_OK || server->systemId == NULL) {
+        goto DONE;
     }
 
     UtoolRedfishGet(server, "/Systems/%s/LogServices", NULL, NULL, result);
@@ -92,30 +103,41 @@ int UtoolCmdGetEventLog(UtoolCommandOption *commandOption, char **outputStr)
     getLogServicesJson = result->data;
 
     cJSON *logService0 = cJSONUtils_GetPointer(getLogServicesJson, "/Members/0/@odata.id");
-    ret = UtoolAssetJsonNodeNotNull(logService0, "/Members/0/@odata.id");
-    if (ret != UTOOLE_OK) {
+    result->code = UtoolAssetJsonNodeNotNull(logService0, "/Members/0/@odata.id");
+    if (result->code != UTOOLE_OK) {
         goto FAILURE;
     }
 
     // get log service 0
     char querySelLogUrl[MAX_URL_LEN];
     char *log0Url = logService0->valuestring;
-    snprintf(querySelLogUrl, MAX_URL_LEN, "%s/Actions/Oem/Huawei/LogService.QuerySelLogEntries", log0Url);
+    snprintf(querySelLogUrl, MAX_URL_LEN, "%s/Actions/Oem/Huawei/LogService.CollectSel", log0Url);
 
-    /** query sel */
-    int currentEntryId = 1;
-    payload = cJSON_CreateObject();
-    cJSON_AddNumberToObject(payload, "StartEntryId", currentEntryId);
-    cJSON_AddNumberToObject(payload, "EntriesCount", 32);
-    UtoolRedfishPost(server, querySelLogUrl, payload, output, getEventArrayMappings, result);
+    UtoolRedfishPost(server, querySelLogUrl, payload, NULL, NULL, result);
+    if (result->interrupt) {
+        goto FAILURE;
+    }
+
+    // waiting util task complete or exception
+    UtoolRedfishWaitUtilTaskFinished(server, result->data, result);
     if (result->interrupt) {
         goto FAILURE;
     }
     FREE_CJSON(result->data)
 
-    // mapping result to output json
-    result->code = UtoolBuildOutputResult(STATE_SUCCESS, output, &(result->desc));
+    ZF_LOGI("export SEL log task finished successfully");
+    if (opt->isLocalFile) { /* download file to local if necessary */
+        ZF_LOGI("Try to download SEL log CSV file from BMC now.");
+        UtoolDownloadFileFromBMC(server, opt->bmcTempFileUrl, opt->exportToFileUrl, result);
+        if (result->interrupt) {
+            goto FAILURE;
+        }
+    }
+
+    // output to outputStr
+    UtoolBuildDefaultSuccessResult(&(result->desc));
     goto DONE;
+
 
 FAILURE:
     FREE_CJSON(output)
@@ -124,8 +146,121 @@ FAILURE:
 DONE:
     FREE_CJSON(payload)
     FREE_CJSON(getLogServicesJson)
+    FREE_OBJ(opt->bmcTempFileUrl)
     UtoolFreeRedfishServer(server);
 
     *outputStr = result->desc;
     return result->code;
+}
+
+
+/**
+* validate user input options for the command
+*
+* @param opt
+* @param resultR
+* @return
+*/
+static void ValidateSubcommandOptions(UtoolGetEventLog *opt, UtoolResult *result)
+{
+    UtoolParsedUrl *parsedUrl = NULL;
+    ZF_LOGI("Export to file URI is %s.", opt->exportToFileUrl);
+
+    if (UtoolStringIsEmpty(opt->exportToFileUrl)) {
+        result->code = UtoolBuildOutputResult(STATE_FAILURE, cJSON_CreateString(OPT_REQUIRED("file-uri")),
+                                              &(result->desc));
+        goto FAILURE;
+    }
+
+    /** parse url */
+    parsedUrl = UtoolParseURL(opt->exportToFileUrl);
+    if (parsedUrl) {
+        opt->isLocalFile = 0;
+        ZF_LOGI("It seems the export to file URI is a network file. protocol is: %s.", parsedUrl->scheme);
+        if (!UtoolStringCaseInArray(parsedUrl->scheme, PROTOCOL_CHOICES)) {
+            ZF_LOGI("Network file protocol %s is not supported.", parsedUrl->scheme);
+            result->code = UtoolBuildOutputResult(STATE_FAILURE, cJSON_CreateString(OPT_FILE_URL_ILLEGAL),
+                                                  &(result->desc));
+            goto FAILURE;
+        }
+    }
+    else {
+        ZF_LOGI("Could not detect schema from export to file URI. Try to treat it as local file.");
+        int fd = open(opt->exportToFileUrl, O_RDWR | O_CREAT, 0664);
+        if (fd == -1) {
+            ZF_LOGI("%s is not a valid local file path.", opt->exportToFileUrl);
+            result->code = UtoolBuildOutputResult(STATE_FAILURE, cJSON_CreateString(OPT_FILE_URL_ILLEGAL),
+                                                  &(result->desc));
+            goto FAILURE;
+        }
+        else {
+            opt->isLocalFile = 1;
+            ZF_LOGI("%s is a valid local file.", opt->exportToFileUrl);
+            if (close(fd) < 0) {
+                ZF_LOGE("Failed to close fd of %s.", opt->exportToFileUrl);
+                result->code = UTOOLE_INTERNAL;
+                goto FAILURE;
+            }
+        }
+    }
+
+    goto DONE;
+
+FAILURE:
+    result->interrupt = 1;
+    goto DONE;
+
+DONE:
+    UtoolFreeParsedURL(parsedUrl);
+}
+
+static cJSON *BuildPayload(UtoolGetEventLog *opt, UtoolResult *result)
+{
+    cJSON *payload = cJSON_CreateObject();
+    result->code = UtoolAssetCreatedJsonNotNull(payload);
+    if (result->code != UTOOLE_OK) {
+        goto FAILURE;
+    }
+
+    cJSON *node = cJSON_AddStringToObject(payload, "Type", "URI");
+    result->code = UtoolAssetCreatedJsonNotNull(node);
+    if (result->code != UTOOLE_OK) {
+        goto FAILURE;
+    }
+
+    if (!opt->isLocalFile) {
+        node = cJSON_AddStringToObject(payload, "Content", opt->exportToFileUrl);
+        result->code = UtoolAssetCreatedJsonNotNull(node);
+        if (result->code != UTOOLE_OK) {
+            goto FAILURE;
+        }
+    }
+    else {
+        ZF_LOGI("Could not detect schema from export to file URI. Try to treat it as local file.");
+        int fd = open(opt->exportToFileUrl, O_RDWR | O_CREAT, 0644);
+        if (fd == -1) {
+            ZF_LOGI("%s is not a valid local file path.", opt->exportToFileUrl);
+            result->code = UtoolBuildOutputResult(STATE_FAILURE, cJSON_CreateString(OPT_FILE_URL_ILLEGAL),
+                                                  &(result->desc));
+            goto FAILURE;
+        }
+        else {
+            ZF_LOGI("%s is a valid local file.", opt->exportToFileUrl);
+            char *filename = basename(opt->exportToFileUrl);
+            opt->bmcTempFileUrl = (char *) malloc(PATH_MAX);
+            snprintf(opt->bmcTempFileUrl, PATH_MAX, "/tmp/web/%s", filename);
+            node = cJSON_AddStringToObject(payload, "Content", opt->bmcTempFileUrl);
+            result->code = UtoolAssetCreatedJsonNotNull(node);
+            if (result->code != UTOOLE_OK) {
+                goto FAILURE;
+            }
+        }
+    }
+
+    return payload;
+
+FAILURE:
+    result->interrupt = 1;
+    FREE_CJSON(payload)
+    return NULL;
 }
